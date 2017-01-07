d4009e37140489efba4b1c591d6756daa64e4ace
Remi <remi@localhost.localdomain>
2017-01-07 14:33:51 +0100

    essais divers et variés

--- penDetector.py
+++ penDetector.py
@@ -25,12 +25,16 @@ def intersectionsP(lines, height, width):
 	if lines is not None:
 		for line in lines:
 			x1,y1,x2,y2 = line[0]
-			vec = (x2-x1, y2-y1) 
-			pt2 = (x2+3*vec[0],y2+3*vec[1])
-			pt1 = (x1-3*vec[0],y1-3*vec[1]) 
+			vec = [x2-x1, y2-y1] 
+			norm = np.sqrt(vec[0]*vec[0]+vec[1]*vec[1])
+			vec[0] = vec[0]/norm
+			vec[1] = vec[1]/norm
+			coeff = 150
+			pt2 = (int(x2+coeff*vec[0]),int(y2+coeff*vec[1]))
+			pt1 = (int(x1-coeff*vec[0]),int(y1-coeff*vec[1])) 
 			
 			tmpFrame = np.zeros((height,width,1), dtype = "uint8")
-			cv2.line(tmpFrame,pt1,pt2,10,2)
+			cv2.line(tmpFrame,pt1,pt2,20,2)
 			frameIntersectionsP = cv2.add(frameIntersectionsP, tmpFrame)
 	return frameIntersectionsP
 
@@ -44,12 +48,12 @@ def brightestSpotsImg(img, nbSpots):
 		imgCopy[maxLoc[1],maxLoc[0]] = 0
 		if maxLoc[1] > 0:
 			imgCopy[maxLoc[1]-1,maxLoc[0]] = 0
-		if maxLoc[1] < width:
-			imgCopy[maxLoc[1]+1,maxLoc[0]] = 0
-		if maxLoc[0] > 0:
-			imgCopy[maxLoc[1],maxLoc[0]-1] = 0
-		if maxLoc[0] < height:
-			imgCopy[maxLoc[1],maxLoc[0]+1] = 0
+	#	if maxLoc[1] < width-1:
+	#		imgCopy[maxLoc[1]+1,maxLoc[0]] = 0
+	#	if maxLoc[0] > 0:
+	#		imgCopy[maxLoc[1],maxLoc[0]-1] = 0
+	#	if maxLoc[0] < height-1:
+	#		imgCopy[maxLoc[1],maxLoc[0]+1] = 0
 		
 	return spotsImg
 
@@ -58,6 +62,7 @@ def frameTreatment(frame, prevFrame):
 	frameGray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 	frameHSL = cv2.cvtColor(frame, cv2.COLOR_BGR2HLS)
 	frameHue = frame[:,:,0]
+	frameHSLGray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 	frameLightness = frame[:,:,1]
 	frameSaturation = frame[:,:,2]
 
@@ -67,15 +72,27 @@ def frameTreatment(frame, prevFrame):
 	diffGray = cv2.equalizeHist(diffGray)
 	ret, diffGrayBin = cv2.threshold(diffGray,68, 255, cv2.THRESH_BINARY)
 
-	frameDiff = cv2.bitwise_and(frame,frame,mask=diffGrayBin) 
-	diffCanny = cv2.Canny(frameDiff, threshold1=100, threshold2=500, apertureSize=3)
+	frameLaplacian = cv2.Laplacian(frameHSLGray, -1)
+	#ret, frameLaplacian = cv2.threshold(frameLaplacian,5, 255, cv2.THRESH_BINARY)
+	frameCanny = cv2.Canny(frameGray, threshold1=10, threshold2=150, apertureSize=3)
+	frameCannyDiff = cv2.bitwise_and(frameCanny,frameCanny,mask=diffGrayBin) 
+	frameLaplacianDiff = cv2.bitwise_and(frameLaplacian,frameLaplacian,mask=diffGrayBin)
 
-#	lines = cv2.HoughLinesP(diffCanny,1,np.pi/180,threshold=20,minLineLength=10,maxLineGap=5)
-	lines = cv2.HoughLines(diffCanny,1,np.pi/180,threshold=30)
-	frameIntersections = intersections(lines, frameHeight, frameWidth)	
+	lines = cv2.HoughLinesP(frameLaplacianDiff,1,np.pi/180,threshold=10,minLineLength=10,maxLineGap=1)
+#	lines = cv2.HoughLines(diffCanny,1,np.pi/180,threshold=20)
+	frameIntersections = intersectionsP(lines, frameHeight, frameWidth)	
+
+	#lines = cv2.HoughLinesP(frameIntersections,1,np.pi/180,threshold=100,minLineLength=10,maxLineGap=10)
+	#frameIntersections2 = intersectionsP(lines, frameHeight, frameWidth)
 	frameIntersectionsBlur = cv2.GaussianBlur(frameIntersections,(33,33),0)
+
+	backImg = cv2.cvtColor(frameIntersections, cv2.COLOR_GRAY2BGR)
 	spotsImg = brightestSpotsImg(frameIntersectionsBlur, 10)
-	spotsImg = cv2.add(spotsImg, diff)
+	spotsImg = cv2.add(spotsImg,backImg)
+	img = cv2.cvtColor(frameIntersections, cv2.COLOR_GRAY2BGR)
+	img[:,:,0] = 0
+	#img2 = cv2.cvtColor(frameIntersections2, cv2.COLOR_GRAY2BGR)
+	#img = cv2.add(img,img2)
 	img=spotsImg
 	cv2.imshow('window', img)
 
